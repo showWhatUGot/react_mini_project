3360ba061c7e56ad1ea873f118442339a067974d
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-02 04:32:13 +0900

    Js 기능 구현 + css 디자인

---
+++ to_do_list/src/ToDoList.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./ToDoList.css";
+
+const ToDoList = () => {
+  const [items, setItem] = useState([
+    { id: 1, text: "아침에 사과 먹기" },
+    { id: 2, text: "운동하기" },
+    { id: 3, text: "리액트 공부하기" },
+  ]);
+  const [inputTxt, setInput] = useState("");
+  const [newId, setNewId] = useState(4);
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onClick = () => {
+    const newItems = items.concat({
+      id: newId,
+      text: inputTxt,
+    });
+    setNewId(newId + 1);
+    setItem(newItems);
+    setInput("");
+  };
+
+  const onPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  const onRemove = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItem(newItems);
+  };
+
+  const mkList = items.map((item) => (
+    <li id="itemList" key={item.id}>
+      {item.text}
+      <button id="btNextItem" onClick={() => onRemove(item.id)}>
+        ❌
+      </button>
+    </li>
+  ));
+
+  return (
+    <div id="mainDiv">
+      <div id="inputDiv">
+        <input
+          id="inputTag"
+          value={inputTxt}
+          onChange={onChange}
+          onKeyPress={onPress}
+          placeholder="할 일을 입력하세요."
+        />
+        <button onClick={onClick}>추가</button>
+      </div>
+      <div>
+        <ul>{mkList}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
